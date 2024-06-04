cdc9f6ccac636c59999b6579fad81f40d41e34fe
Kai <khsauian@gmail.com>
2024-06-04 22:03:06 +0800

    feat: timetable v0.1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { Link, Tabs } from 'expo-router'
+import { Pressable } from 'react-native'
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import Colors from '@/constants/Colors'
+import { useColorScheme } from '@/components/useColorScheme'
+import { useClientOnlyValue } from '@/components/useClientOnlyValue'
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
+  name: React.ComponentProps<typeof FontAwesome>['name']
+  color: string
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
 
   return (
     <Tabs
@@ -25,12 +25,25 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+        tabBarStyle: { height: 100 },
+      }}
+    >
+      <Tabs.Screen
+        name="cctv"
+        options={{
+          title: '即時影像',
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="video-camera" color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: '時刻表',
+          tabBarIcon: ({ color }) => <TabBarIcon name="train" color={color} />,
+          tabBarLabel: '',
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -48,19 +61,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="weather"
         options={{
           title: '目前天氣',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarLabel: '',
+          tabBarIcon: ({ color }) => <TabBarIcon name="sun-o" color={color} />,
         }}
       />
     </Tabs>
-  );
+  )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,305 @@
-import { StyleSheet } from 'react-native'
-
-import EditScreenInfo from '@/components/EditScreenInfo'
+import React from 'react'
+import {
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+  Pressable,
+  Dimensions,
+} from 'react-native'
+import { Picker } from '@react-native-picker/picker'
 import { Text, View } from '@/components/Themed'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
-export default function TabOneScreen() {
+interface AuthResponse {
+  access_token: string
+  token_type: string
+  expires_in: number
+  scope: string
+}
+
+async function GetAuthorizationHeader(): Promise<AuthResponse | undefined> {
+  const parameter = new URLSearchParams({
+    grant_type: 'client_credentials',
+    client_id: 'sssun-09d597db-5ec8-446e',
+    client_secret: '8ffe4bd6-dc2e-40e1-8f9e-2c5d62e13ab1',
+  })
+
+  const auth_url =
+    'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token'
+
+  try {
+    const response = await fetch(auth_url, {
+      method: 'POST',
+      headers: {
+        'Accept-Encoding': 'br,gzip',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: parameter.toString(),
+    })
+
+    if (response.ok) {
+      const data: AuthResponse = await response.json()
+      return data
+    } else {
+      console.error('Authorization Error:', response.statusText)
+    }
+  } catch (error) {
+    console.error('Authorization Error:', error)
+  }
+}
+
+async function GetApiResponse(
+  accesstoken: AuthResponse | undefined,
+  setData: Function
+): Promise<void> {
+  if (!accesstoken) {
+    console.error('No access token found')
+    return
+  }
+
+  try {
+    const response = await fetch(
+      'https://tdx.transportdata.tw/api/basic/v3/Rail/AFR/GeneralTrainTimetable',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + accesstoken.access_token,
+          'Accept-Encoding': 'br,gzip',
+        },
+      }
+    )
+
+    if (response.ok) {
+      const data = await response.json()
+      const timetable = data['TrainTimetables']
+      const schedules = timetable.reduce((acc: any, schedule: any) => {
+        schedule.StopTimes.forEach((stop: any) => {
+          if (!acc[stop.StationID]) {
+            acc[stop.StationID] = {
+              StationName: stop.StationName,
+              Times: [],
+            }
+          }
+          acc[stop.StationID].Times.push({
+            TrainNo: schedule.TrainNo,
+            StopSequence: stop.StopSequence,
+            ArrivalTime: stop.ArrivalTime,
+            DepartureTime: stop.DepartureTime,
+          })
+        })
+        return acc
+      }, {})
+      console.log('API Response:', schedules)
+      setData(schedules)
+      // 在這裡處理數據，設置狀態或顯示在界面上
+    } else {
+      console.error('API Response Error:', response.statusText)
+    }
+  } catch (error) {
+    console.error('API Response Error:', error)
+  }
+}
+export default function TimeTableScreen() {
+  const [data, setData] = React.useState<any>(null)
+  const [startingStation, setStartingStation] = React.useState<string>('嘉義')
+  const [endingStation, setEndingStation] = React.useState<string>('祝山')
+  const [startingStationTmp, setStartingStationTmp] =
+    React.useState<string>('嘉義')
+  const [endingStationTmp, setEndingStationTmp] = React.useState<string>('祝山')
+  const [isModalVisible, setIsModalVisible] = React.useState<boolean>(false)
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const token = await GetAuthorizationHeader()
+      await GetApiResponse(token, setData)
+    }
+    fetchData()
+  }, [])
+  const stationNames = data
+    ? Object.values(data).map((station: any) => station.StationName)
+    : []
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible)
+  }
+  const exchangeStation = () => {
+    let tmp = startingStation
+    setStartingStation(endingStation)
+    setEndingStation(tmp)
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-    </View>
+    <>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <Image
+          style={{ width: '100%', height: 120 }}
+          resizeMode="cover"
+          source={{
+            uri: 'https://cpok.tw/wp-content/uploads/2022/03/unnamed-file-42.png',
+          }}
+        />
+        <View style={styles.infoBox}>
+          <View
+            style={{
+              height: Dimensions.get('screen').height * 0.15,
+              width: '100%',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              paddingHorizontal: '10%',
+              paddingVertical: '5%',
+              backgroundColor: 'transparent',
+            }}
+          >
+            <View style={{ height: '100%', alignItems: 'flex-start' }}>
+              <Text style={{ color: '#767676', marginVertical: 15 }}>
+                起程站
+              </Text>
+              <Pressable onPress={toggleModal}>
+                <Text style={{ fontSize: 25 }}>{startingStation}</Text>
+              </Pressable>
+            </View>
+            <Pressable onPress={exchangeStation} style={{ top: 10 }}>
+              <FontAwesome size={25} name="exchange" color={'#767676'} />
+            </Pressable>
+            <View style={{ height: '100%', alignItems: 'flex-end' }}>
+              <Text style={{ color: '#767676', marginVertical: 15 }}>
+                終點站
+              </Text>
+              <Pressable onPress={toggleModal}>
+                <Text style={{ fontSize: 25 }}>{endingStation}</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+      {isModalVisible && (
+        <View style={styles.pickerContainer}>
+          <View style={styles.pickerTopBar}>
+            <TouchableOpacity
+              onPress={() => {
+                let tmp = startingStationTmp
+                setStartingStationTmp(endingStationTmp)
+                setEndingStationTmp(tmp)
+              }}
+            >
+              <Text style={[styles.buttonText, { color: '#28B67E' }]}>
+                起迄站互換
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                if (startingStationTmp !== endingStationTmp) {
+                  setStartingStation(startingStationTmp)
+                  setEndingStation(endingStationTmp)
+                  toggleModal()
+                } else {
+                  Alert.alert('起程站與到達站不能相同')
+                }
+              }}
+            >
+              <Text style={[styles.buttonText, { color: '#28B67E' }]}>
+                完成
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flex: 1, paddingTop: 13 }}>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  color: 'gray',
+                  fontSize: 18,
+                  fontWeight: '400',
+                }}
+              >
+                起程站
+              </Text>
+              <Picker
+                selectedValue={startingStationTmp}
+                onValueChange={(itemValue) => setStartingStationTmp(itemValue)}
+              >
+                {stationNames.map((stationName: any, index: number) => (
+                  <Picker.Item
+                    key={index}
+                    label={stationName.Zh_tw}
+                    value={stationName.Zh_tw}
+                  />
+                ))}
+              </Picker>
+            </View>
+            <View style={{ flex: 1, paddingTop: 13 }}>
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  color: 'gray',
+                  fontSize: 18,
+                  fontWeight: '400',
+                }}
+              >
+                到達站
+              </Text>
+              <Picker
+                selectedValue={endingStationTmp}
+                onValueChange={(itemValue) => setEndingStationTmp(itemValue)}
+              >
+                {stationNames.map((stationName: any, index: number) => (
+                  <Picker.Item
+                    key={index}
+                    label={stationName.Zh_tw}
+                    value={stationName.Zh_tw}
+                  />
+                ))}
+              </Picker>
+            </View>
+          </View>
+        </View>
+      )}
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  infoBox: {
+    height: Dimensions.get('screen').height * 0.5,
+    width: '90%',
+    borderRadius: 15,
+    shadowColor: 'rgba(0,0,0,0.25)', // Shadow color
+    shadowOpacity: 1, // Shadow opacity (0.0 to 1.0)
+    shadowOffset: { width: 0, height: 2 }, // Shadow offset (width, height)
+    shadowRadius: 4, // Shadow blur radius
+    elevation: 5, // Android shadow elevation (works on Android API level 21 and above)
+  },
+  contentContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  pickerContainer: {
+    width: '100%',
+    // padding: 16,
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  pickerTopBar: {
+    flexDirection: 'row',
+    height: '20%',
+    width: '100%',
+    borderBottomWidth: 0.5,
+    borderColor: 'gray',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 25,
   },
   title: {
     fontSize: 20,
@@ -30,5 +309,11 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    letterSpacing: 1,
   },
 })
